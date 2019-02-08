79b78516a2c9cec56129bc664469ef76da67b462
水野　響 <hibiki990523@gmail.com>
2019-02-08 17:10:00 +0900

    small changes

--- temperatureDataOfJanuary.py
+++ temperatureDataOfJanuary.py
@@ -9,8 +9,7 @@ df = pd.read_csv("data.csv",encoding="shift_jis",skiprows=[0,1,2,4,5],index_col=
  
 df = df.drop(df.columns[[1,2]],axis=1)
 
-list_day = [i for i in range(1,32)]
-x = np.array(list_day)
+x = np.linspace(1, 31, num = 31, endpoint = True, retstep = False, dtype = None)
 
 Y2009 = np.array(df.loc["2009/1/1":"2009/1/31","C"])
 Y2010 = np.array(df.loc["2010/1/1":"2010/1/31","C"])
@@ -22,7 +21,6 @@ Y2015 = np.array(df.loc["2015/1/1":"2015/1/31","C"])
 Y2016 = np.array(df.loc["2016/1/1":"2016/1/31","C"])
 Y2017 = np.array(df.loc["2017/1/1":"2017/1/31","C"])
 Y2018 = np.array(df.loc["2018/1/1":"2018/1/31","C"])
-
 
 
 dataframe = pd.DataFrame({
